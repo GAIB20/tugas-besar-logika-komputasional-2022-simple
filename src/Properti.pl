dc5f7013bc8078928687347cd3435cf59cc00931
[SulthanDA28] <[13521159@std.stei.itb.ac.id]>
2022-11-15 21:11:17 +0700

    feat: menambahkan properti

---
+++ src/Properti.pl
@@ -0,0 +1,372 @@
+/*Fakta*/
+
+
+bangunan(a1).
+bangunan(a2).
+bangunan(a3).
+bangunan(b1).
+bangunan(b2).
+bangunan(b3).
+bangunan(c1).
+bangunan(c2).
+bangunan(c3).
+bangunan(d1).
+bangunan(d2).
+bangunan(d3).
+bangunan(e1).
+bangunan(e2).
+bangunan(e3).
+bangunan(f1).
+bangunan(f2).
+bangunan(f3).
+bangunan(g1).
+bangunan(g2).
+bangunan(g3).
+bangunan(h1).
+bangunan(h2).
+
+/*Nama Kota*/
+nama(a1,'Jakarta').
+nama(a2,'Kuala Lumpur').
+nama(a3,'Singapura').
+nama(b1,'Phnom Penh').
+nama(b2,'Manila').
+nama(b3,'Bangkok').
+nama(c1,'Dili').
+nama(c2,'Vientiane').
+nama(c3,'Hanoi').
+nama(d1,'Beijing').
+nama(d2,'Hongkong').
+nama(d3,'Macau').
+nama(e1,'Taipei').
+nama(e2,'Tokyo').
+nama(e3,'Seoul').
+nama(f1,'Pyongyang').
+nama(f2,'Dhaka').
+nama(f3,'New Delhi').
+nama(g1,'Kathmandu').
+nama(g2,'Islamabad').
+nama(g3,'Riyadh').
+nama(h1,'Kuwait').
+nama(h2,'Doha').
+
+/*Deskripsi*/
+deskripsi(a1,'Ibukota Indonesia').
+deskripsi(a2,'Ibukota Malaysia').
+deskripsi(a3,'Ibukota Singapura').
+deskripsi(b1,'Ibukota Kamboja').
+deskripsi(b2,'Ibukota Filipina').
+deskripsi(b3,'Ibukota Thailand').
+deskripsi(c1,'Ibukota Timor Leste').
+deskripsi(c2,'Ibukota Laos').
+deskripsi(c3,'Ibukota Vietnam').
+deskripsi(d1,'Ibukota China').
+deskripsi(d2,'Ibukota Hongkong').
+deskripsi(d3,'Ibukota Makau').
+deskripsi(e1,'Ibukota Taiwan').
+deskripsi(e2,'Ibukota Jepang').
+deskripsi(e3,'Ibukota Korea Selatan').
+deskripsi(f1,'Ibukota Korea Utara').
+deskripsi(f2,'Ibukota Bangladesh').
+deskripsi(f3,'Ibukota India').
+deskripsi(g1,'Ibukota Nepal').
+deskripsi(g2,'Ibukota Pakistan').
+deskripsi(g3,'Ibukota Arab Saudi').
+deskripsi(h1,'Ibukota Kuwait').
+deskripsi(h2,'Ibukota Qatar').
+
+
+/*Daftar Properti*/
+
+/*Jakarta*/
+properti(a1,tanah,200).
+properti(a1,bangunan1,3000).
+properti(a1,bangunan2,4000).
+properti(a1,bangunan3,5000).
+properti(a1,landmark,5200).
+properti(a1,sewatanah,60).
+properti(a1,sewa1,150).
+properti(a1,sewa2,200).
+properti(a1,sewa3,400).
+properti(a1,sewalandmark,800).
+
+/*Kuala Lumpur*/
+properti(a2,tanah,250).
+properti(a2,bangunan1,3100).
+properti(a2,bangunan2,3900).
+properti(a2,bangunan3,4500).
+properti(a2,landmark,4600).
+properti(a2,sewatanah,70).
+properti(a2,sewa1,150).
+properti(a2,sewa2,220).
+properti(a2,sewa3,300).
+properti(a2,sewalandmark,600).
+
+/*Singapura*/
+properti(a3,tanah,500).
+properti(a3,bangunan1,4000).
+properti(a3,bangunan2,4500).
+properti(a3,bangunan3,4900).
+properti(a3,landmark,5500).
+properti(a3,sewatanah,150).
+properti(a3,sewa1,400).
+properti(a3,sewa2,500).
+properti(a3,sewa3,600).
+properti(a3,sewalandmark,1000).
+
+/*Phnom Penh*/
+properti(b1,tanah,300).
+properti(b1,bangunan1,3200).
+properti(b1,bangunan2,3500).
+properti(b1,bangunan3,3900).
+properti(b1,landmark,4100).
+properti(b1,sewatanah,80).
+properti(b1,sewa1,120).
+properti(b1,sewa2,240).
+properti(b1,sewa3,350).
+properti(b1,sewalandmark,700).
+
+/*Manila*/
+properti(b2,tanah,290).
+properti(b2,bangunan1,2900).
+properti(b2,bangunan2,3100).
+properti(b2,bangunan3,3500).
+properti(b2,landmark,3700).
+properti(b2,sewatanah,70).
+properti(b2,sewa1,110).
+properti(b2,sewa2,230).
+properti(b2,sewa3,340).
+properti(b2,sewalandmark,600).
+
+/*Bangkok*/
+properti(b3,tanah,400).
+properti(b3,bangunan1,3200).
+properti(b3,bangunan2,3400).
+properti(b3,bangunan3,3500).
+properti(b3,landmark,3800).
+properti(b3,sewatanah,70).
+properti(b3,sewa1,340).
+properti(b3,sewa2,400).
+properti(b3,sewa3,520).
+properti(b3,sewalandmark,700).
+
+/*Dili*/
+properti(c1,tanah,400).
+properti(c1,bangunan1,3200).
+properti(c1,bangunan2,3400).
+properti(c1,bangunan3,3500).
+properti(c1,landmark,4000).
+properti(c1,sewatanah,50).
+properti(c1,sewa1,200).
+properti(c1,sewa2,230).
+properti(c1,sewa3,300).
+properti(c1,sewalandmark,550).
+
+/*Vientiane*/
+properti(c2,tanah,320).
+properti(c2,bangunan1,3400).
+properti(c2,bangunan2,3500).
+properti(c2,bangunan3,3600).
+properti(c2,landmark,3800).
+properti(c2,sewatanah,30).
+properti(c2,sewa1,160).
+properti(c2,sewa2,240).
+properti(c2,sewa3,320).
+properti(c2,sewalandmark,560).
+
+/*Hanoi*/
+properti(c3,tanah,460).
+properti(c3,bangunan1,3800).
+properti(c3,bangunan2,4000).
+properti(c3,bangunan3,4300).
+properti(c3,landmark,4500).
+properti(c3,sewatanah,60).
+properti(c3,sewa1,310).
+properti(c3,sewa2,410).
+properti(c3,sewa3,500).
+properti(c3,sewalandmark,650).
+
+/*Beijing*/
+properti(d1,tanah,270).
+properti(d1,bangunan1,2800).
+properti(d1,bangunan2,2900).
+properti(d1,bangunan3,3200).
+properti(d1,landmark,3500).
+properti(d1,sewatanah,90).
+properti(d1,sewa1,200).
+properti(d1,sewa2,250).
+properti(d1,sewa3,300).
+properti(d1,sewalandmark,560).
+
+/*Hongkong*/
+properti(d2,tanah,280).
+properti(d2,bangunan1,2800).
+properti(d2,bangunan2,3000).
+properti(d2,bangunan3,3300).
+properti(d2,landmark,3600).
+properti(d2,sewatanah,80).
+properti(d2,sewa1,120).
+properti(d2,sewa2,240).
+properti(d2,sewa3,330).
+properti(d2,sewalandmark,570).
+
+/*Macau*/
+properti(d3,tanah,390).
+properti(d3,bangunan1,3000).
+properti(d3,bangunan2,3200).
+properti(d3,bangunan3,3500).
+properti(d3,landmark,3900).
+properti(d3,sewatanah,70).
+properti(d3,sewa1,280).
+properti(d3,sewa2,360).
+properti(d3,sewa3,380).
+properti(d3,sewalandmark,570).
+
+/*Taipei*/
+properti(e1,tanah,360).
+properti(e1,bangunan1,3300).
+properti(e1,bangunan2,3400).
+properti(e1,bangunan3,3600).
+properti(e1,landmark,4200).
+properti(e1,sewatanah,80).
+properti(e1,sewa1,200).
+properti(e1,sewa2,240).
+properti(e1,sewa3,310).
+properti(e1,sewalandmark,560).
+
+/*Tokyo*/
+properti(e2,tanah,500).
+properti(e2,bangunan1,4500).
+properti(e2,bangunan2,4900).
+properti(e2,bangunan3,5700).
+properti(e2,landmark,6000).
+properti(e2,sewatanah,120).
+properti(e2,sewa1,790).
+properti(e2,sewa2,850).
+properti(e2,sewa3,900).
+properti(e2,sewalandmark,1500).
+
+/*Seoul*/
+properti(e3,tanah,460).
+properti(e3,bangunan1,4300).
+properti(e3,bangunan2,4700).
+properti(e3,bangunan3,5200).
+properti(e3,landmark,5800).
+properti(e3,sewatanah,120).
+properti(e3,sewa1,700).
+properti(e3,sewa2,840).
+properti(e3,sewa3,880).
+properti(e3,sewalandmark,1300).
+
+/*Pyongyang*/
+properti(f1,tanah,400).
+properti(f1,bangunan1,3900).
+properti(f1,bangunan2,4300).
+properti(f1,bangunan3,4500).
+properti(f1,landmark,4900).
+properti(f1,sewatanah,100).
+properti(f1,sewa1,800).
+properti(f1,sewa2,830).
+properti(f1,sewa3,850).
+properti(f1,sewalandmark,1000).
+
+/*Dhaka*/
+properti(f2,tanah,390).
+properti(f2,bangunan1,3000).
+properti(f2,bangunan2,3200).
+properti(f2,bangunan3,3400).
+properti(f2,landmark,3700).
+properti(f2,sewatanah,70).
+properti(f2,sewa1,200).
+properti(f2,sewa2,260).
+properti(f2,sewa3,320).
+properti(f2,sewalandmark,620).
+
+/*New Delhi*/
+properti(f3,tanah,310).
+properti(f3,bangunan1,2800).
+properti(f3,bangunan2,3000).
+properti(f3,bangunan3,3200).
+properti(f3,landmark,3500).
+properti(f3,sewatanah,30).
+properti(f3,sewa1,230).
+properti(f3,sewa2,360).
+properti(f3,sewa3,400).
+properti(f3,sewalandmark,500).
+
+/*Kathmandu*/
+properti(g1,tanah,350).
+properti(g1,bangunan1,3000).
+properti(g1,bangunan2,3100).
+properti(g1,bangunan3,3300).
+properti(g1,landmark,3700).
+properti(g1,sewatanah,70).
+properti(g1,sewa1,280).
+properti(g1,sewa2,300).
+properti(g1,sewa3,320).
+properti(g1,sewalandmark,650).
+
+/*Islamabad*/
+properti(g2,tanah,310).
+properti(g2,bangunan1,3400).
+properti(g2,bangunan2,3500).
+properti(g2,bangunan3,3700).
+properti(g2,landmark,4000).
+properti(g2,sewatanah,50).
+properti(g2,sewa1,350).
+properti(g2,sewa2,400).
+properti(g2,sewa3,430).
+properti(g2,sewalandmark,520).
+
+/*Riyadh*/
+properti(g3,tanah,340).
+properti(g3,bangunan1,3300).
+properti(g3,bangunan2,3500).
+properti(g3,bangunan3,3800).
+properti(g3,landmark,4000).
+properti(g3,sewatanah,80).
+properti(g3,sewa1,300).
+properti(g3,sewa2,390).
+properti(g3,sewa3,420).
+properti(g3,sewalandmark,700).
+
+/*Kuwait*/
+properti(h1,tanah,430).
+properti(h1,bangunan1,4000).
+properti(h1,bangunan2,4200).
+properti(h1,bangunan3,4500).
+properti(h1,landmark,4900).
+properti(h1,sewatanah,160).
+properti(h1,sewa1,540).
+properti(h1,sewa2,600).
+properti(h1,sewa3,670).
+properti(h1,sewalandmark,870).
+
+/*Doha*/
+properti(h2,tanah,370).
+properti(h2,bangunan1,2600).
+properti(h2,bangunan2,2900).
+properti(h2,bangunan3,3200).
+properti(h2,landmark,3500).
+properti(h2,sewatanah,90).
+properti(h2,sewa1,240).
+properti(h2,sewa2,350).
+properti(h2,sewa3,410).
+properti(h2,sewalandmark,670).
+
+
+cekPropertyDetail(X) :-
+print('Nama Properti         : '),nama(X,Name),print(Name),print('\n'),
+print('Deskripsi Properti    : '),deskripsi(X,Desk),print(Desk),print('\n'),
+print('\n'),
+print('Harga Tanah           : '),properti(X,tanah,Tnh),print(Tnh),print('\n'),
+print('Harga Bangunan 1      : '),properti(X,bangunan1,X1),print(X1),print('\n'),
+print('Harga Bangunan 2      : '),properti(X,bangunan2,X2),print(X2),print('\n'),
+print('Harga Bangunan 3      : '),properti(X,bangunan3,X3),print(X3),print('\n'),
+print('Harga Landmark        : '),properti(X,landmark,X4),print(X4),print('\n'),
+print('\n'),
+print('Harga Sewa Tanah      : '),properti(X,sewatanah,Y1),print(Y1),print('\n'),
+print('Harga Sewa Bangunan 1 : '),properti(X,sewa1,Y2),print(Y2),print('\n'),
+print('Harga Sewa Bangunan 2 : '),properti(X,sewa2,Y3),print(Y3),print('\n'),
+print('Harga Sewa Bangunan 3 : '),properti(X,sewa3,Y4),print(Y4),print('\n'),
+print('Harga Sewa Landmark   : '),properti(X,sewalandmark,Y5),print(Y5),print('\n').
