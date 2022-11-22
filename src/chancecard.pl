a190ad2a27cff7b46d738b9efcd81c49ced6b1bd
Agilham <13521118@std.stei.itb.ac.id>
2022-11-22 20:38:37 +0700

    feat: menambahkan fakta dan rule chancecard dan penjara

--- src/chancecard.pl
+++ src/chancecard.pl
@@ -1,18 +1,58 @@
-/* Rencana Fakta */
-:- dynamic (listcard/1)
-chancecard(taxcard).
-chancecard(giftcard).
-chancecard(injailcard).
-chancecard(outjailcard).
+/* Fakta */
+
+% card1 adalah kartu yang saat ini dibaca dari listcard1
+:- dynamic(card1/1).
+% card2 adalah kartu yang saat ini dibaca dari listcard2
+:- dynamic(card2/1).
+
+% chancecard(1) adalah tax card. Pemain dipindahkan ke petak pajak.
+chancecard(1).
+% chancecard(2) adalah gift card. Pemain mendapatkan hadiah berupa uang dengan jumlah tertentu.
+chancecard(2).
+% chancecard(3) adalah get in jail card. Pemain dipindahkan ke petak penjara.
+chancecard(3).
+% chancecard(4) adalah get out of out jail card. Pemain dapat keluar ari petak penjara jika menggunakan kartu ini.
+chancecard(4).
+% chancecard(5) adalah angel card. Pemain dapat menggunakan angel card untuk melindungi dari bayar sewa atau properti dibeli.
+
+% giftamount(10000) adalah total hadiah uang yang akan didapat oleh pemain jika mendapat kartu gift card
 giftamount(10000).
 
-/* Rencana Rules */
+/* Rules */
+
+% rollnumber(X) bernilai true dengan X adalah angka hasil fungsi random
 rollnumber(X) :- random(1, 100, X).
-rollcard(X) :- rollnumber(Y), Y>0, Y=<15, X=1.
-rollcard(X) :- rollnumber(Y), Y>15, Y=<50, X=2.
-rollcard(X) :- rollnumber(Y), Y>50, Y=<65, X=3.
-rollcard(X) :- rollnumber(Y), Y>65, Y=<100, X=4.
+
+% rollcard(X) bernilai true dengan X adalah 1, 2, 3, 4, atau 5 berdasarkan hasil rollnumber(Y)
+rollcard(X) :- rollnumber(Y), Y>0, Y=<20, X=1.
+rollcard(X) :- rollnumber(Y), Y>20, Y=<45, X=2.
+rollcard(X) :- rollnumber(Y), Y>45, Y=<65, X=3.
+rollcard(X) :- rollnumber(Y), Y>65, Y=<90, X=4.
+rollcard(X) :- rollnumber(Y), Y>90, Y=<100, X=5.
+
+% cardresult(X) bernilai true jika rollcard(Y), maka X adalah Y.
 cardresult(X) :- rollcard(Y), Y =:= 1, X = Y.
 cardresult(X) :- rollcard(Y), Y =:= 2, X = Y.
 cardresult(X) :- rollcard(Y), Y =:= 3, X = Y.
 cardresult(X) :- rollcard(Y), Y =:= 4, X = Y.
+cardresult(X) :- rollcard(Y), Y =:= 5, X = Y.
+
+% printcard(Y) menampilkan keterangan kartu apa yang didapat oleh pemain
+printcard(Y) :- cardresult(Y), Y =:= 1,
+write('Anda mendapat kartu tax card. Anda akan dipinidahkan ke petak pajak.').
+printcard(Y) :- cardresult(Y), Y =:= 2,
+write('Anda mendapat kartu gift card. Anda akan mendapatkan uang sebesar 10000.').
+printcard(Y) :- cardresult(Y), Y =:= 3,
+write('Anda mendapat kartu get in jail card. Anda akan dipindahkan ke petak penjara.').
+printcard(Y) :- cardresult(Y), Y =:= 4,
+write('Anda mendapat kartu get out of jail card. Simpan kartu ini agar bisa digunakan.').
+printcard(Y) :- cardresult(Y), Y =:= 5,
+write('Anda mendapat kartu angel card. Simpan kartu ini agar bisa digunakan.')
+
+% addcard(X) menambahkan kartu X ke dalam listcard
+addcard1(X) :- printcard(Y), card1(Y), assertz(listcard1(Y)).
+addcard2(X) :- printcard(Y), card2(Y), assertz(listcard2(Y)).
+
+% removecard(X) mengambil kartu X dari dalam listcard
+removecard1(X) :- card1(X), retract(listcard1(X)).
+removecard2(X) :- card2(X), retract(listcard2(X)).
--- src/penjara.pl
+++ src/penjara.pl
@@ -1,9 +1,69 @@
-/* Rencana Fakta */
-:- dynamic (turncount/1).
+:- include(chancecard.pl).
+
+/* Fakta */
+
+% turncount1 menyimpan jumlah turn pemain1 ada di penjara
+:- dynamic(turncount1/1).
+$ turncount2 menyimpan jumlah turn pemain2 ada di penjara
+:- dynamic(turncount2/1).
+
+% fineamount(5000) adalah total uang denda yang harus dibayar jika pemain ingin keluar dair penjara
 fineamount(5000).
 
-/* Rencana Rules */
-bayardenda(X) :- pemain(X), uang(Y), fineamount(Z), Y > Z.
-pakaikartu(X) :- pemain(X), cardlist(Y), chancecard(Z), Z =:= outjailcard.
-keluarpenjara(X) :- bayardenda(A); pakaikartu(B);
-turncount(C), A =:= 3; double(D).
+% penjara1(0) adalah keadaan pemain1 tidak berada di dalam penjara
+penjara1(0).
+% penjara1(1) adalah keadaan pemain1 berada di dalam penjara
+penjara1(1).
+% penjara2(0) adalah keadaan pemain2 tidak berada di dalam penjara
+penjara2(0)
+% penjara2(1) adalah kondisi pemain2 berada di dalam penjara
+penjara2(1).
+
+/* Rules */
+
+% checkpenjara(X) bernilai trua jika pemain berada di dalam penjara
+checkpenjara1(X) :- penjara1(X), X =:= 1.
+checkpenjara2(X) :- penjara2(X), X =:= 1.
+
+% checkkartu(X) bernilai true jika pemain memiliki kartu get out of jail
+checkkartu1(X) :- checkpenjara1(Y), Y =:= 1, retract(listcard1), card1(X), X =:= 4.
+checkkartu2(X) :- checkpenjara2(Y), Y =:= 1, retract(listcard2), card2(X), X =:= 4.
+
+% pakaikartu(X) bernilai true jika pemain memilih untuk menggunakan kartu get out of jail
+pakaikartu1(X) :- checkkartu1(Y), removecard1(Y).
+pakaikartu2(X) :- checkkartu2(Y), removecard2(Y).
+
+% checkuang(X) bernilai true jika pemain memiliki uang lebih besar dari denda
+checkuang1(x) :- checkpenjara1(Y), Y =:= 1, retract(uangpemain1), uangpemain1 > fineamount, asserta(uangpemain1), X=uangpemain1.
+checkuang2(x) :- checkpenjara2(Y), Y =:= 1, retract(uangpemain2), uangpemain2 > fineamount, asserta(uangpemain2), X=uangpemain2.
+
+% bayardenda(X) bernilai true jika pemain memilih untuk membayar denda
+bayardenda1(X) :- checkuang1(Y), retract(uangpemain1), uangpemain1 is Y-fineamount, asserta(uangpemain1).
+bayardenda2(X) :- checkuang2(Y), retract(uangpemain2), uangpemain1 is Y-fineamount, asserta(uangpemain2).
+
+
+% checkdice(X) bernilai true jika hasil lemparan dadu double
+checkdice1(X) :- checkpenjara1(Y), Y =:= 1, retract(dice1pemain1), retract(dice2pemain1), dice1pemain1 =:= 6, dice2pemain1 =:= 6,
+assertz(dice1pemain1), assertz(dice2pemain1).
+checkdice2(X) :- checkpenjara1(Y), Y =:= 1, retract(dice1pemain2), retract(dice2pemain2), dice1pemain2 =:= 6, dice2pemain2 =:= 6,
+assertz(dice1pemain2), assertz(dice2pemain2).
+
+% checckturn(X) bernilai true jika turn pemain berada di dalam penjara kurang dari 3
+checkturn1(X) :- checkpenjara1(Y), Y =:= 1, retract(turncount1), turncount1=<3, assertz(turncount1).
+checkturn2(X) :- checkpenjara1(Y), Y =:= 1, retract(turncount2), turncount2=<3, assertz(turncount2).
+
+% turnplus(X) bernilai true jika pemain tidak memilih pakaikartu, bayardenda, tidak mendapat double, dan checkturn bernilai true
+turnplus1(X) :- checkturn1(Y) , \+pakaikartu1(A), \+bayardenda1(B), \+ checkdice1(C),
+retract(turncount1), turncount1 is turncount1+1, assertz(turncount1).
+turnplus2(X) :- checkturn2(Y) , \+pakaikartu2(A), \+bayardenda2(B), \+ checkdice2(C),
+retract(turncount2), turncount2 is turncount2+1, assertz(turncount2).
+
+% keluarpenjara(X) bernilai true jika melakukan aksi pakaikartu, bayardenda, mendapat double, atau telah menunggu 3 kali giliran.
+keluarpenjara1(X) :- pakaikartu1(Y), penjara1(Z), Z=0.
+keluarpenjara1(X) :- bayardenda1(Y), penjara1(Z), Z=0.
+keluarpenjara1(X) :- checkdice1(Y), penjara1(Z), Z=0.
+keluarpenjara1(X) :- \+checkturn1(Y), penjara1(Z), Z=0.
+keluarpenjara2(X) :- pakaikartu2(Y), penjara2(Z), Z=0.
+keluarpenjara2(X) :- bayardenda2(Y), penjara2(Z), Z=0.
+keluarpenjara2(X) :- checkdice2(Y), penjara2(Z), Z=0.
+keluarpenjara2(X) :- \+checkturn2(Y), penjara2(Z), Z=0.
