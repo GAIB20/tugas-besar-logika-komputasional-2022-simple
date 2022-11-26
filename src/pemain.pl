1b59aa00d9e70147586571580c273eefff3f0f72
shelmasalsa17 <98295919+shelmasalsa17@users.noreply.github.com>
2022-11-27 06:07:32 +0700

    pemain

--- src/pemain.pl
+++ src/pemain.pl
@@ -1,48 +1,52 @@
 :- dynamic(player/7).
 :- dynamic(curPlayer/1).
-:- dynamic(addProperti/3).
-:- dynamic(addKartu/2).
+:- dynamic(punyaproperti/3).
+:- dynamic(punyakartu/2).
 :- dynamic(cnt/1).
 :- dynamic(cntcard/1).
+:- dynamic(dadu1/1).
+:- dynamic(dadu2/1).
+:- dynamic(count/1).
 
 
-
-
-
-/*Fungsi-Fungsi yang akan digunakan*/ 
+/*Fungsi init pemain adalah menginitkan semua nilai-nilai awal dan menset curPlayer  yang akan pertama kali bermain adalah pemain 1*/
 
 initPemain :- 
-            asserta(player('Player 1', 'GO',50,0,50,0,0)), 
-            asserta(player('Player 2', 'GO',50,0,50,0,0)),
-            asserta(curPlayer('Player 1')).
+            asserta(player('Player 1', 'GO',50,0,50)), 
+            asserta(player('Player 2', 'GO',50,0,50)),
+            asserta(curPlayer('Player 1')),assertz(count(0)).
 
-gantiPemain :- retract(count(_)),curPlayer(P),retract(curPlayer(P)),((P = 'Player 1'-> assertz(curPlayer('Player 2')));assertz(curPlayer('Player 1'))),assertz(count(0)).
-infoPemain :- curPlayer(P),player(P, Lokasi, Totaluang, Totalnilaiproperti,Totalaset,Properti,Daftarkartu),
-                                    write('your status : '), write(P), nl,
-                                    write('lokasi : '), write(Lokasi), nl,
-                                    write('total uang: '), write(Totaluang), nl,
-                                    write('totalnilaiproperti: '), write(Totalnilaiproperti), nl,
-                                    write('totalaset: '), write(Totalaset), nl,
-                                    write('Properti yang dimiliki: '), write(Properti), nl,
-                                    write('Daftar kartu yang dimiliki: '), write(Daftarkartu),nl.
+/*Fungsi ganti pemain untuk mengganti pemain jika awalnya pemain 1 maka akan menjadi pemain 2 dan sebalikna */
+gantiPemain :- retract(count(_)),assertz(count(0)),curPlayer(P),retract(curPlayer(P)),((P = 'Player 1'-> assertz(curPlayer('Player 2')));assertz(curPlayer('Player 1'))).
+/*Untuk mengecek detail kepemilikan suatu pemain*/
+checkPlayerDetail:- curPlayer(P),player(P, Lokasi, Totaluang, Totalnilaiproperti,Totalaset),write('Informasi Player '), write('('),write(P),write(')'),nl,nl,
+                                write('Lokasi                    :'), write(Lokasi),nl,
+                                write('Total Uang                :'), write(Totaluang),nl,
+                                write('Total Nilai Properti      :'), write(Totalnilaiproperti),nl,
+                                write('Total Aset                :'), write(Totalaset),nl,nl,
+                                printAllProperti,nl,nl,printAllCard.
 
-ubahnilaiproperti(X) :- curPlayer(P), retract(player(P,B,C,D,E,F,G)), Z is D+X,K is F+1, assertz(player(P,B,C,Z,E,K,G)).
-beliProperti(X) :- curPlayer(P),retract(player(P,B,C,D,E,F,G)),Z is C-X,assertz(player(P,B,Z,D,E,F,G)),ubahnilaiproperti(X).
-ubahLokasi(X) :- curPlayer(P),retract(player(P,_,C,D,E,F,G)),assertz(player(P,X,C,D,E,F,G)).
+/*Membeli sebuah properti X sebagai info player 1 atau 2 eg. 'Player 1' kemudian Y adalah Properti yang akan dibeli Z adalah jenis properti dan K harga properti*/
+beliProperti(X,Y,Z,K) :- curPlayer(P),retract(player(P,B,C,D,E)),L is C-K,H is D+K,addProperti(X,Y,Z),T is L+H,assertz(player(P,B,L,H,T)).
+ubahLokasi(X) :- curPlayer(P),retract(player(P,B,C,D,E)),assertz(player(P,X,C,D,E)).
 
-
-/*Untuk properti*/
-
+/*Fungsi untuk menambah properti X : info player 1 atau 2 'Player 1', Y adalah nama properti dan Z jenis properti kayak 'Bangunan'*/
 addProperti(X,Y,Z) :- assertz(punyaproperti(X,Y,Z)).
+/*Menambah kartu X info pemain dan Y nama kartu nya*/
+addKartu(X,Y) :- assertz(punyakartu(X,Y)).
 
-printAll :- assertz(cnt(1)), 
-    forall((addProperti(X,Y,Z)), 
-        (cnt(Cnt), print(Cnt), print('. '), print(' '), print(Y), print(' '),print(Z),nl,
+/*Print properti yang dimiliki*/
+printAllProperti :- write('Daftar Kepemilikan Properti :'),nl,assertz(cnt(1)), 
+    forall((punyaproperti(X,Y,Z)), 
+        (cnt(Cnt), write(Cnt), write('. '), write(' '), write(Y), write(' '),write('-'),write(' '),write(Z),nl,
         retract(cnt(Cnt)), Cnt1 is Cnt+1, assertz(cnt(Cnt1)))).
-
-addKartu(X,Y) :- assertz(cntcard(1)),
-    forall((addKartu(X,Y)), 
+/*print kartu yang dimiliki*/
+printAllCard :- write('Daftar Kepemilikan Card :'),nl,assertz(cntcard(1)),
+    forall((punyakartu(X,Y)), 
         (cntcard(Cnt), print(Cnt), print('. '), print(' '), print(Y),nl,
         retract(cntcard(Cnt)), Cnt1 is cntcard+1, assertz(cntcard(Cnt1)))).
 
-
+/*throw dice*/
+throwDice :- curPlayer(P), write('Sekarang Giliran '),write(P),random(1,7,X), random(1,7,Y),nl,nl,write('dadu 1 : '),write(X),write('.'),nl,
+write('dadu 2 : '),write(Y),write('.'),nl, Z is X+Y, write('Anda maju sejauh '), write(Z), write(' langkah'),
+((Y =:= X -> write('double'), retract(count(A)), Z is A+1, assertz(count(Z)),((Z =:= 3 -> write('Anda masuk penjara'), gantiPemain);throwDice));gantiPemain).
