251cfdce0bb7c9d92b54aa02e7296e03fd227b33
Muhammad Habibi Husni <habibihusni18@gmail.com>
2022-11-26 23:48:07 +0700

    refactor: ubah fakta properti menjadi beli dan ewa

--- src/Properti.pl
+++ src/Properti.pl
@@ -4,294 +4,294 @@
 /*Daftar Properti*/
 
 /*Jakarta*/
-properti(a1,tanah,200).
-properti(a1,bangunan1,3000).
-properti(a1,bangunan2,4000).    
-properti(a1,bangunan3,5000).
-properti(a1,landmark,5200).
-properti(a1,sewatanah,60).
-properti(a1,sewa1,150).
-properti(a1,sewa2,200).
-properti(a1,sewa3,400).
-properti(a1,sewalandmark,800).
+beli(a1,0,200).
+beli(a1,1,3000).
+beli(a1,2,4000).
+beli(a1,3,5000).
+beli(a1,l,5200).
+sewa(a1,0,60).
+sewa(a1,1,150).
+sewa(a1,2,200).
+sewa(a1,3,400).
+sewa(a1,l,800).
 
 /*Kuala Lumpur*/
-properti(a2,tanah,250).
-properti(a2,bangunan1,3100).
-properti(a2,bangunan2,3900).
-properti(a2,bangunan3,4500).
-properti(a2,landmark,4600).
-properti(a2,sewatanah,70).
-properti(a2,sewa1,150).
-properti(a2,sewa2,220).
-properti(a2,sewa3,300).
-properti(a2,sewalandmark,600).
+beli(a2,0,250).
+beli(a2,1,3100).
+beli(a2,2,3900).
+beli(a2,3,4500).
+beli(a2,l,4600).
+sewa(a2,0,70).
+sewa(a2,1,150).
+sewa(a2,2,220).
+sewa(a2,3,300).
+sewa(a2,l,600).
 
 /*Singapura*/
-properti(a3,tanah,500).
-properti(a3,bangunan1,4000).
-properti(a3,bangunan2,4500).
-properti(a3,bangunan3,4900).
-properti(a3,landmark,5500).
-properti(a3,sewatanah,150).
-properti(a3,sewa1,400).
-properti(a3,sewa2,500).
-properti(a3,sewa3,600).
-properti(a3,sewalandmark,1000).
+beli(a3,0,500).
+beli(a3,1,4000).
+beli(a3,2,4500).
+beli(a3,3,4900).
+beli(a3,l,5500).
+sewa(a3,0,150).
+sewa(a3,1,400).
+sewa(a3,2,500).
+sewa(a3,3,600).
+sewa(a3,l,1000).
 
 /*Phnom Penh*/
-properti(b1,tanah,300).
-properti(b1,bangunan1,3200).           
-properti(b1,bangunan2,3500).
-properti(b1,bangunan3,3900).
-properti(b1,landmark,4100).
-properti(b1,sewatanah,80).
-properti(b1,sewa1,120).
-properti(b1,sewa2,240).
-properti(b1,sewa3,350).
-properti(b1,sewalandmark,700).
+beli(b1,0,300).
+beli(b1,1,3200).
+beli(b1,2,3500).
+beli(b1,3,3900).
+beli(b1,l,4100).
+sewa(b1,0,80).
+sewa(b1,1,120).
+sewa(b1,2,240).
+sewa(b1,3,350).
+sewa(b1,l,700).
 
 /*Manila*/
-properti(b2,tanah,290).
-properti(b2,bangunan1,2900).
-properti(b2,bangunan2,3100).
-properti(b2,bangunan3,3500).
-properti(b2,landmark,3700).
-properti(b2,sewatanah,70).
-properti(b2,sewa1,110).
-properti(b2,sewa2,230).
-properti(b2,sewa3,340).
-properti(b2,sewalandmark,600).
+beli(b2,0,290).
+beli(b2,1,2900).
+beli(b2,2,3100).
+beli(b2,3,3500).
+beli(b2,l,3700).
+sewa(b2,0,70).
+sewa(b2,1,110).
+sewa(b2,2,230).
+sewa(b2,3,340).
+sewa(b2,l,600).
 
 /*Bangkok*/
-properti(b3,tanah,400).
-properti(b3,bangunan1,3200).
-properti(b3,bangunan2,3400).
-properti(b3,bangunan3,3500).
-properti(b3,landmark,3800).
-properti(b3,sewatanah,70).
-properti(b3,sewa1,340).
-properti(b3,sewa2,400).
-properti(b3,sewa3,520).
-properti(b3,sewalandmark,700).
+beli(b3,0,400).
+beli(b3,1,3200).
+beli(b3,2,3400).
+beli(b3,3,3500).
+beli(b3,l,3800).
+sewa(b3,0,70).
+sewa(b3,1,340).
+sewa(b3,2,400).
+sewa(b3,3,520).
+sewa(b3,l,700).
 
 /*Dili*/
-properti(c1,tanah,400).
-properti(c1,bangunan1,3200).
-properti(c1,bangunan2,3400).
-properti(c1,bangunan3,3500).
-properti(c1,landmark,4000).
-properti(c1,sewatanah,50).
-properti(c1,sewa1,200).
-properti(c1,sewa2,230).
-properti(c1,sewa3,300).
-properti(c1,sewalandmark,550).
+beli(c1,0,400).
+beli(c1,1,3200).
+beli(c1,2,3400).
+beli(c1,3,3500).
+beli(c1,l,4000).
+sewa(c1,0,50).
+sewa(c1,1,200).
+sewa(c1,2,230).
+sewa(c1,3,300).
+sewa(c1,l,550).
 
 /*Vientiane*/
-properti(c2,tanah,320).
-properti(c2,bangunan1,3400).
-properti(c2,bangunan2,3500).
-properti(c2,bangunan3,3600).
-properti(c2,landmark,3800).
-properti(c2,sewatanah,30).
-properti(c2,sewa1,160).
-properti(c2,sewa2,240).
-properti(c2,sewa3,320).
-properti(c2,sewalandmark,560).
+beli(c2,0,320).
+beli(c2,1,3400).
+beli(c2,2,3500).
+beli(c2,3,3600).
+beli(c2,l,3800).
+sewa(c2,0,30).
+sewa(c2,1,160).
+sewa(c2,2,240).
+sewa(c2,3,320).
+sewa(c2,l,560).
 
 /*Hanoi*/
-properti(c3,tanah,460).
-properti(c3,bangunan1,3800).
-properti(c3,bangunan2,4000).
-properti(c3,bangunan3,4300).
-properti(c3,landmark,4500).
-properti(c3,sewatanah,60).
-properti(c3,sewa1,310).
-properti(c3,sewa2,410).
-properti(c3,sewa3,500).
-properti(c3,sewalandmark,650).
+beli(c3,0,460).
+beli(c3,1,3800).
+beli(c3,2,4000).
+beli(c3,3,4300).
+beli(c3,l,4500).
+sewa(c3,0,60).
+sewa(c3,1,310).
+sewa(c3,2,410).
+sewa(c3,3,500).
+sewa(c3,l,650).
 
 /*Beijing*/
-properti(d1,tanah,270).
-properti(d1,bangunan1,2800).
-properti(d1,bangunan2,2900).
-properti(d1,bangunan3,3200).
-properti(d1,landmark,3500).
-properti(d1,sewatanah,90).
-properti(d1,sewa1,200).
-properti(d1,sewa2,250).
-properti(d1,sewa3,300).
-properti(d1,sewalandmark,560).
+beli(d1,0,270).
+beli(d1,1,2800).
+beli(d1,2,2900).
+beli(d1,3,3200).
+beli(d1,l,3500).
+sewa(d1,0,90).
+sewa(d1,1,200).
+sewa(d1,2,250).
+sewa(d1,3,300).
+sewa(d1,l,560).
 
 /*Hongkong*/
-properti(d2,tanah,280).
-properti(d2,bangunan1,2800).
-properti(d2,bangunan2,3000).
-properti(d2,bangunan3,3300).
-properti(d2,landmark,3600).
-properti(d2,sewatanah,80).
-properti(d2,sewa1,120).
-properti(d2,sewa2,240).
-properti(d2,sewa3,330).
-properti(d2,sewalandmark,570).
+beli(d2,0,280).
+beli(d2,1,2800).
+beli(d2,2,3000).
+beli(d2,3,3300).
+beli(d2,l,3600).
+sewa(d2,0,80).
+sewa(d2,1,120).
+sewa(d2,2,240).
+sewa(d2,3,330).
+sewa(d2,l,570).
 
 /*Macau*/
-properti(d3,tanah,390).
-properti(d3,bangunan1,3000).
-properti(d3,bangunan2,3200).
-properti(d3,bangunan3,3500).
-properti(d3,landmark,3900).
-properti(d3,sewatanah,70).
-properti(d3,sewa1,280).
-properti(d3,sewa2,360).
-properti(d3,sewa3,380).
-properti(d3,sewalandmark,570).
+beli(d3,0,390).
+beli(d3,1,3000).
+beli(d3,2,3200).
+beli(d3,3,3500).
+beli(d3,l,3900).
+sewa(d3,0,70).
+sewa(d3,1,280).
+sewa(d3,2,360).
+sewa(d3,3,380).
+sewa(d3,l,570).
 
 /*Taipei*/
-properti(e1,tanah,360).
-properti(e1,bangunan1,3300).
-properti(e1,bangunan2,3400).
-properti(e1,bangunan3,3600).
-properti(e1,landmark,4200).
-properti(e1,sewatanah,80).
-properti(e1,sewa1,200).
-properti(e1,sewa2,240).
-properti(e1,sewa3,310).
-properti(e1,sewalandmark,560).
+beli(e1,0,360).
+beli(e1,1,3300).
+beli(e1,2,3400).
+beli(e1,3,3600).
+beli(e1,l,4200).
+sewa(e1,0,80).
+sewa(e1,1,200).
+sewa(e1,2,240).
+sewa(e1,3,310).
+sewa(e1,l,560).
 
 /*Tokyo*/
-properti(e2,tanah,500).
-properti(e2,bangunan1,4500).
-properti(e2,bangunan2,4900).
-properti(e2,bangunan3,5700).
-properti(e2,landmark,6000).
-properti(e2,sewatanah,120).
-properti(e2,sewa1,790).
-properti(e2,sewa2,850).
-properti(e2,sewa3,900).
-properti(e2,sewalandmark,1500).
+beli(e2,0,500).
+beli(e2,1,4500).
+beli(e2,2,4900).
+beli(e2,3,5700).
+beli(e2,l,6000).
+sewa(e2,0,120).
+sewa(e2,1,790).
+sewa(e2,2,850).
+sewa(e2,3,900).
+sewa(e2,l,1500).
 
 /*Seoul*/
-properti(e3,tanah,460).
-properti(e3,bangunan1,4300).
-properti(e3,bangunan2,4700).
-properti(e3,bangunan3,5200).
-properti(e3,landmark,5800).
-properti(e3,sewatanah,120).
-properti(e3,sewa1,700).
-properti(e3,sewa2,840).
-properti(e3,sewa3,880).
-properti(e3,sewalandmark,1300).
+beli(e3,0,460).
+beli(e3,1,4300).
+beli(e3,2,4700).
+beli(e3,3,5200).
+beli(e3,l,5800).
+sewa(e3,0,120).
+sewa(e3,1,700).
+sewa(e3,2,840).
+sewa(e3,3,880).
+sewa(e3,l,1300).
 
 /*Pyongyang*/
-properti(f1,tanah,400).
-properti(f1,bangunan1,3900).
-properti(f1,bangunan2,4300).
-properti(f1,bangunan3,4500).
-properti(f1,landmark,4900).
-properti(f1,sewatanah,100).
-properti(f1,sewa1,800).
-properti(f1,sewa2,830).
-properti(f1,sewa3,850).
-properti(f1,sewalandmark,1000).
+beli(f1,0,400).
+beli(f1,1,3900).
+beli(f1,2,4300).
+beli(f1,3,4500).
+beli(f1,l,4900).
+sewa(f1,0,100).
+sewa(f1,1,800).
+sewa(f1,2,830).
+sewa(f1,3,850).
+sewa(f1,l,1000).
 
 /*Dhaka*/
-properti(f2,tanah,390).
-properti(f2,bangunan1,3000).
-properti(f2,bangunan2,3200).
-properti(f2,bangunan3,3400).
-properti(f2,landmark,3700).
-properti(f2,sewatanah,70).
-properti(f2,sewa1,200).
-properti(f2,sewa2,260).
-properti(f2,sewa3,320).
-properti(f2,sewalandmark,620).
+beli(f2,0,390).
+beli(f2,1,3000).
+beli(f2,2,3200).
+beli(f2,3,3400).
+beli(f2,l,3700).
+sewa(f2,0,70).
+sewa(f2,1,200).
+sewa(f2,2,260).
+sewa(f2,3,320).
+sewa(f2,l,620).
 
 /*New Delhi*/
-properti(f3,tanah,310).
-properti(f3,bangunan1,2800).
-properti(f3,bangunan2,3000).
-properti(f3,bangunan3,3200).
-properti(f3,landmark,3500).
-properti(f3,sewatanah,30).
-properti(f3,sewa1,230).
-properti(f3,sewa2,360).
-properti(f3,sewa3,400).
-properti(f3,sewalandmark,500).
+beli(f3,0,310).
+beli(f3,1,2800).
+beli(f3,2,3000).
+beli(f3,3,3200).
+beli(f3,l,3500).
+sewa(f3,0,30).
+sewa(f3,1,230).
+sewa(f3,2,360).
+sewa(f3,3,400).
+sewa(f3,l,500).
 
 /*Kathmandu*/
-properti(g1,tanah,350).
-properti(g1,bangunan1,3000).
-properti(g1,bangunan2,3100).
-properti(g1,bangunan3,3300).
-properti(g1,landmark,3700).
-properti(g1,sewatanah,70).
-properti(g1,sewa1,280).
-properti(g1,sewa2,300).
-properti(g1,sewa3,320).
-properti(g1,sewalandmark,650).
+beli(g1,0,350).
+beli(g1,1,3000).
+beli(g1,2,3100).
+beli(g1,3,3300).
+beli(g1,l,3700).
+sewa(g1,0,70).
+sewa(g1,1,280).
+sewa(g1,2,300).
+sewa(g1,3,320).
+sewa(g1,l,650).
 
 /*Islamabad*/
-properti(g2,tanah,310).
-properti(g2,bangunan1,3400).
-properti(g2,bangunan2,3500).
-properti(g2,bangunan3,3700).
-properti(g2,landmark,4000).
-properti(g2,sewatanah,50).
-properti(g2,sewa1,350).
-properti(g2,sewa2,400).
-properti(g2,sewa3,430).
-properti(g2,sewalandmark,520).
+beli(g2,0,310).
+beli(g2,1,3400).
+beli(g2,2,3500).
+beli(g2,3,3700).
+beli(g2,l,4000).
+sewa(g2,0,50).
+sewa(g2,1,350).
+sewa(g2,2,400).
+sewa(g2,3,430).
+sewa(g2,l,520).
 
 /*Riyadh*/
-properti(g3,tanah,340).
-properti(g3,bangunan1,3300).
-properti(g3,bangunan2,3500).
-properti(g3,bangunan3,3800).
-properti(g3,landmark,4000).
-properti(g3,sewatanah,80).
-properti(g3,sewa1,300).
-properti(g3,sewa2,390).
-properti(g3,sewa3,420).
-properti(g3,sewalandmark,700).
+beli(g3,0,340).
+beli(g3,1,3300).
+beli(g3,2,3500).
+beli(g3,3,3800).
+beli(g3,l,4000).
+sewa(g3,0,80).
+sewa(g3,1,300).
+sewa(g3,2,390).
+sewa(g3,3,420).
+sewa(g3,l,700).
 
 /*Kuwait*/
-properti(h1,tanah,430).
-properti(h1,bangunan1,4000).
-properti(h1,bangunan2,4200).
-properti(h1,bangunan3,4500).
-properti(h1,landmark,4900).
-properti(h1,sewatanah,160).
-properti(h1,sewa1,540).
-properti(h1,sewa2,600).
-properti(h1,sewa3,670).
-properti(h1,sewalandmark,870).
+beli(h1,0,430).
+beli(h1,1,4000).
+beli(h1,2,4200).
+beli(h1,3,4500).
+beli(h1,l,4900).
+sewa(h1,0,160).
+sewa(h1,1,540).
+sewa(h1,2,600).
+sewa(h1,3,670).
+sewa(h1,l,870).
 
 /*Doha*/
-properti(h2,tanah,370).
-properti(h2,bangunan1,2600).
-properti(h2,bangunan2,2900).
-properti(h2,bangunan3,3200).
-properti(h2,landmark,3500).
-properti(h2,sewatanah,90).
-properti(h2,sewa1,240).
-properti(h2,sewa2,350).
-properti(h2,sewa3,410).
-properti(h2,sewalandmark,670).
+beli(h2,0,370).
+beli(h2,1,2600).
+beli(h2,2,2900).
+beli(h2,3,3200).
+beli(h2,l,3500).
+sewa(h2,0,90).
+sewa(h2,1,240).
+sewa(h2,2,350).
+sewa(h2,3,410).
+sewa(h2,l,670).
 
 
 cekPropertyDetail(X) :-
 print('Nama Properti         : '),nama(X,Name),print(Name),print('\n'),
 print('Deskripsi Properti    : '),deskripsi(X,Desk),print(Desk),print('\n'),
 print('\n'),
-print('Harga Tanah           : '),properti(X,tanah,Tnh),print(Tnh),print('\n'),
-print('Harga Bangunan 1      : '),properti(X,bangunan1,X1),print(X1),print('\n'),
-print('Harga Bangunan 2      : '),properti(X,bangunan2,X2),print(X2),print('\n'),
-print('Harga Bangunan 3      : '),properti(X,bangunan3,X3),print(X3),print('\n'),
-print('Harga Landmark        : '),properti(X,landmark,X4),print(X4),print('\n'),
+print('Harga tanah           : '),beli(X,0,Tnh),print(Tnh),print('\n'),
+print('Harga Bangunan 1      : '),beli(X,1,X1),print(X1),print('\n'),
+print('Harga Bangunan 2      : '),beli(X,2,X2),print(X2),print('\n'),
+print('Harga Bangunan 3      : '),beli(X,3,X3),print(X3),print('\n'),
+print('Harga landmark        : '),beli(X,l,X4),print(X4),print('\n'),
 print('\n'),
-print('Harga Sewa Tanah      : '),properti(X,sewatanah,Y1),print(Y1),print('\n'),
-print('Harga Sewa Bangunan 1 : '),properti(X,sewa1,Y2),print(Y2),print('\n'),
-print('Harga Sewa Bangunan 2 : '),properti(X,sewa2,Y3),print(Y3),print('\n'),
-print('Harga Sewa Bangunan 3 : '),properti(X,sewa3,Y4),print(Y4),print('\n'),
-print('Harga Sewa Landmark   : '),properti(X,sewalandmark,Y5),print(Y5),print('\n').
+print('Harga Sewa tanah      : '),sewa(X,0,Y1),print(Y1),print('\n'),
+print('Harga Sewa Bangunan 1 : '),sewa(X,1,Y2),print(Y2),print('\n'),
+print('Harga Sewa Bangunan 2 : '),sewa(X,2,Y3),print(Y3),print('\n'),
+print('Harga Sewa Bangunan 3 : '),sewa(X,3,Y4),print(Y4),print('\n'),
+print('Harga Sewa landmark   : '),sewa(X,l,Y5),print(Y5),print('\n').
