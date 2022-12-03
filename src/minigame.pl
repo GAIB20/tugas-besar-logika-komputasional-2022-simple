802fefe0f51f0176d47e1666513134924d8dd0ac
shelmasalsa17 <98295919+shelmasalsa17@users.noreply.github.com>
2022-12-03 20:29:24 +0700

    feat minigame

--- src/minigame.pl
+++ src/minigame.pl
@@ -7,11 +7,12 @@ salah :- nl,write('Nice try again'),nl,minigame.
 
 
 minigame :- write('Hallo ANDA Sudah di MINIGAME INPUTKAN TEBAKAN anda (range 1-10)'),retract(countguess(A)),C is A+1,assertz(countguess(C)),
-random(1,11,X),nl,write('You guess :  '),read(Z),
+random(1,11,X),nl,write(X),nl,write('You guess :  '),read(Z),
 (X =:= Z -> (write('You right'),retract(youget(D)),F is D+X,assertz(youget(F)),(C =:= 3 -> (nl,write('GG'),nl,
 writehasilminigame);salah));(write('Wrong ANswer'),nl,writehasilminigame)).
 
 
-writehasilminigame :- curPlayer(P),youget(A),write('You get '),write(A),write(' koin'),getmoneypemain(P,S),X is S+A,ubahMoney(X).
+writehasilminigame :- curPlayer(P),youget(F),write('You get '),write(F),write(' koin'),getmoneypemain(P,S),X is S+F,ubahMoney(X),
+retract(countguess(_)),retract(youget(_)).
 checkMiniGame :- initAwal,curPlayer(P),getlokasipemain(P,Lokasi),Lokasi = 'MG',minigame.
 checkMiniGame.
